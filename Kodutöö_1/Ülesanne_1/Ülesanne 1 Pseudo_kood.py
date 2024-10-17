c248449398897ff3dbe797d754dea2cf337595a2
Gasolin21 <150619740+Gasolin21@users.noreply.github.com>
2024-10-17 12:38:59 +0300

    Add files via upload

---
+++ Kodutöö_1/Ülesanne_1/Ülesanne 1 Pseudo_kood.py
@@ -0,0 +1,13 @@
+Algoritm JärjestaRaamatudKõrguseJärgi:
+    Sisend: raamatute_kogumik (list raamatuid, kus iga raamatul on määratud kõrgus)
+    Väljund: raamatute_kogumik järjestatuna kõrguse järgi
+
+    1. Kui raamatute_kogumik on tühi või sisaldab ainult ühte raamatut, siis lõpeta.
+    2. Määra raamatute_kogumiku suurus N.
+    3. Kasutada näiteks mullisortimise (Bubble Sort) algoritmi raamatute järjestamiseks:
+        a. Kordab järgmisi samme N korda:
+            i.  Käia kõik raamatud läbi (järjest 1. raamatust kuni N-1. raamatuni).
+            ii. Kui praeguse raamatu kõrgus on suurem kui järgmise raamatu kõrgus, vaheta nad omavahel.
+    4. Tagasta raamatute_kogumik järjestatuna kasvavas järjekorras (madalaimast kõrgeimani).
+    
+Lõpp
